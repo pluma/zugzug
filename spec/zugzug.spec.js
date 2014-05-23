764ce548d439f9901d8bc5863a05bf80bddf342b
Alan Plum <me@pluma.io>
2014-05-23 13:43:22 +0200

    Test ZugZug constructor.

--- spec/zugzug.spec.js
+++ spec/zugzug.spec.js
@@ -1,8 +1,14 @@
-/*global describe: false, it: false */
 var expect = require('expect.js');
+var ZugZug = require('../lib/zugzug');
 
-describe('this project\'s test-suite', function() {
-  it('must be written', function() {
-    expect(false).to.equal(true);
+describe('new ZugZug([options])', function() {
+  it('is a constructor', function() {
+    var zz = new ZugZug();
+    expect(zz).to.be.a(ZugZug);
+  });
+  it('can be called as a function', function() {
+    var zugzug = ZugZug;
+    var zz = zugzug();
+    expect(zz).to.be.a(ZugZug);
   });
 });
